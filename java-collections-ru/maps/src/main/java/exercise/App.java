1cb57639acdfd482a170bab65bcaaa04605909a6
SciLejkeee <parfenov.anatoly@mail.ru>
2024-02-13 05:27:34 +0500

    Maps is done

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static Map<String, Integer> getWordCount(String sentence) {
+        if (sentence.isEmpty()) return new HashMap<>();
+
+        String[] splitSentence = sentence.split(" ");
+        Map<String, Integer> wordsCount = new LinkedHashMap<>();
+        for (String s : splitSentence) {
+            wordsCount.put(s, (wordsCount.getOrDefault(s, 0) + 1));
+        }
+        return wordsCount;
+    }
+
+    public static String toString(Map<String, Integer> hashMap) {
+        if (HashMap.isEmpty()) return "{}";
+        StringBuilder result = new StringBuilder("{\n");
+        for (Map.Entry<String, Integer> newHashMap : hashMap.entrySet()) {
+            result.append(" ").append(newHashMap.getKey()).append(": ").append(newHashMap.getValue()).append("\n");
+        }
+        result.append("}");
+        return result.toString();
+    }
+}
